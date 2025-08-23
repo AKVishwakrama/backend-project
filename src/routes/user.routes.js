8a8e22b157cce97c673f62a74e083048ec2fafd2
riya-kaurav <kauravriya665@gmail.com>
2025-08-23 23:41:23 +0530

    aggregeation pipelines

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,9 @@
 import {Router} from "express";
 import {registerUser} from "../controllers/user.controllers.js"
 import {upload} from "../middlewares/multer.middleware.js"
+import { loginUser , logoutUser , refreshAccessToken } from "../controllers/user.controllers.js";
+import { verifyJWT } from "../middlewares/auth.middlewares.js";
+
 
 const router = Router()
 
@@ -23,5 +26,6 @@ router.route("/register").post(
 
     //secured user
     router.route("/logout").post(verifyJWT  , logoutUser)
+    router.route("/refresh-token").post(refreshAccessToken)
 
 export default router;
