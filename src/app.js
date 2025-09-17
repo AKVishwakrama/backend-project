ffb77fdf54b7862cf14efc32b4632abf6023be19
AKVishwakrama <amit2315vishwakarma@gmail.com>
2025-09-17 16:38:45 +0530

    msg

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ app.use(express.static("public"));
 app.use(cookieParser());
 
 import userRouter from "./routes/user.routes.js"
-// routes declaration
+
 app.use("/api/v1/users" , userRouter)
 
 export {app}
